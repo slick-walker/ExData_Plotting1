4824106bb8e7d6934b8bb8ed603525d1f2456ddd
slick-walker <iweissbe@yahoo.com>
2015-07-11 20:12:26 -0400

    Adding plot 2 r script and PNG

--- plot2.R
+++ plot2.R
@@ -4,6 +4,9 @@ setwd("C:/Classes/COURSERA/2015 Summer/Exploratory Data Analysis/Projects/Projec
 # Load the data table library.
 library(data.table)
 
+# Load the datasets library.
+library(datasets)
+
 # Read in the data from the file.
 data <- read.table("./household_power_consumption.txt", 
                    sep = ";", 
@@ -16,12 +19,20 @@ newdata <- subset(data, Date == '1/2/2007' | Date == '2/2/2007')
 # Convert the subset to a data table.
 febData <- as.data.table(newdata)
 
-# Create a histogram of global active power.
-hist(febData$Global_active_power,
-     xlab = "Global Active Power (kilowatts)",
-     main = "Global Active Power",
-     col = "red")
+# Copy the dates and times to character vectors.
+s1 <- as.character(febData[[1]])
+s2 <- as.character(febData[[2]])
+s3 <- paste(s1, s2)
+
+# Create a time vector.
+t1 <- strptime(s3, "%d/%m/%Y %H:%M:%S")
+
+# Create the plot.
+plot(t1, febData$Global_active_power,
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)")
 
 # Create the PNG file.
-dev.copy(png, file = "./plot1.png")
+dev.copy(png, file = "./plot2.png")
 dev.off()
